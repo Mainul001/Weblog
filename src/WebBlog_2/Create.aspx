596cb4e9ff4a2dc75067490719da7700e0f24294
Mainul001 <luniam.mi@gmail.com>
2015-03-18 02:50:01 +0600

    Database bug detected

--- src/WebBlog_2/Create.aspx
+++ src/WebBlog_2/Create.aspx
@@ -79,7 +79,7 @@
         <table>
             <tr>
                 <td>
-                    <asp:Label ID="ArticleTempLabel" runat="server" Text=""></asp:Label>
+                    
                 </td>
             </tr>
         </table>
@@ -96,6 +96,8 @@
         </div>
 
         <asp:Button ID="Button1" runat="server" Text="Save" CssClass="btn btn-success btn-lg btn-block" OnClick="Button1_Click" />
+
+        <asp:Label ID="ArticleTempLabel" runat="server" class ="lead" Text=""></asp:Label>
 
     </form>
 
--- src/WebBlog_2/Create.aspx.designer.cs
+++ src/WebBlog_2/Create.aspx.designer.cs
@@ -58,15 +58,6 @@ namespace WebBlog_2 {
         protected global::System.Web.UI.WebControls.TextBox AuthorTextBox;
         
         /// <summary>
-        /// ArticleTempLabel control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label ArticleTempLabel;
-        
-        /// <summary>
         /// htmlEditorTxt control.
         /// </summary>
         /// <remarks>
@@ -83,5 +74,14 @@ namespace WebBlog_2 {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// ArticleTempLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ArticleTempLabel;
     }
 }
