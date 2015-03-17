100c5dc88e86dad7fa42b6eaa5a2c5b294252092
Mainul001 <luniam.mi@gmail.com>
2015-03-18 02:16:51 +0600

    Now users can write articles and it will be stored in the database

--- src/WebBlog_2/Create.aspx.cs
+++ src/WebBlog_2/Create.aspx.cs
@@ -7,6 +7,8 @@ using System.Data;
 using System.Windows.Input;
 using System.Web.UI.WebControls;
 using System.ComponentModel;
+using System.Web.Configuration;
+using System.Data.SqlClient;
 
 namespace WebBlog_2
 {
@@ -19,9 +21,33 @@ namespace WebBlog_2
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            string content = this.htmlEditorTxt.Text;
-            ArticleTempLabel.Text = content;
+            //string content = this.htmlEditorTxt.Text;
+            //ArticleTempLabel.Text = content;
+
+            NewArticle anArticle = new NewArticle(TitleTextBox.Text, AuthorTextBox.Text, htmlEditorTxt.Text);
+            if (SaveArticle(anArticle)) {
+                ArticleTempLabel.Text = "Article saved successfully";
+            }
+
+            else {
+                ArticleTempLabel.Text = "An error occured, please try again";
+            }
+        }
+
+        private bool SaveArticle(NewArticle anArticle) {
             
+            string connectionString = WebConfigurationManager.ConnectionStrings["ArticleConnectionString"].ConnectionString;
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            string query = String.Format("INSERT INTO t_article VALUES('{0}','{1}', '{2}', '{3}')", anArticle.Title, anArticle.Body,
+                anArticle.Author, anArticle.DateAndTime);
+
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+            int rowAffected = command.ExecuteNonQuery();
+            connection.Close();
+            return rowAffected > 0;
         }
     }
 }
---
+++ src/WebBlog_2/NewArticle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBlog_2 {
+    public class NewArticle {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Body { get; set; }
+        public DateTime DateAndTime { get; set; }
+
+        public NewArticle(string title, string author, string body) {
+            this.Title = title;
+            this.Author = author;
+            this.Body = body;
+            this.DateAndTime = DateTime.Now;
+        }
+    }
+}
